c634e794f71bfbd371071e4c1e45837ecf383cef
Pedro-A-Mattos <84406132+Pedro-A-Mattos@users.noreply.github.com>
2024-07-04 12:07:24 -0300

    update folders

---
+++ src/utils/browser_automation.py
@@ -0,0 +1,33 @@
+from fastapi import HTTPException
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_webdriver(url: str):
+    """
+    Função que faz web scraping em uma página web especificada pela URL.
+
+    Args:
+        url (str): A URL da página web a ser raspada.
+
+    Returns:
+        str: O conteúdo da página web em formato HTML.
+
+    Raises:
+        HTTPException: Se ocorrer qualquer erro durante o processo de scraping.
+    """
+    try:
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        # Inicia o webdriver do Chrome
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        page_content = driver.page_source
+        driver.quit()
+
+        return page_content
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
